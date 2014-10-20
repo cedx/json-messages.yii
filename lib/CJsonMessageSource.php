06c9ad11c2ccc5dd4d1976ac23a0f58a05fe2ccd
Cédric Belin <cedric@belin.io>
2014-10-20 10:42:28 +0200

    Added CJsonMessageSource class

---
+++ lib/CJsonMessageSource.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Implementation of the `CJsonMessageSource` class.
+ * @module CJsonMessageSource
+ */
+
+/**
+ * Represents a message source that stores translated messages in JSON files.
+ * @class CJsonMessageSource
+ * @extends system.i18n.CPhpMessageSource
+ * @constructor
+ */
+class CJsonMessageSource extends CPhpMessageSource {
+
+  /**
+   * The string prefixed to every cache key in order to avoid name collisions.
+   * @property CACHE_KEY_PREFIX
+   * @type string
+   * @static
+   * @final
+   */
+  const CACHE_KEY_PREFIX='Yii.CJsonMessageSource.';
+
+  /**
+   * Determines the message file name based on the given category and language.
+   * @method getMessageFile
+   * @param {string} $category The category name.
+   * @param {string} $language The language ID.
+   * @return {string} The path of the message file.
+   * @protected
+   */
+  protected function getMessageFile($category, $language) {
+    return preg_replace('/\.php$/i', '.json', parent::getMessageFile($category, $language));
+  }
+
+  /**
+   * Loads the message translation for the specified language and category.
+   * @method loadMessages
+   * @param {string} $category The message category.
+   * @param {string} $language The target language.
+   * @return {array} The loaded messages.
+   * @protected
+   */
+  protected function loadMessages($category, $language) {
+  	$messageFile=$this->getMessageFile($category, $language);
+    
+    $cache=($this->cacheID===false ? null : Yii::app()->getComponent($this->cacheID));
+    $cacheKey=static::CACHE_KEY_PREFIX.$messageFile;
+  	if($cache && $this->cachingDuration>0) {
+  		$data=$cache->get($cacheKey);
+  		if($data!==false) return unserialize($data);
+  	}
+
+  	$messages=(is_file($messageFile) ? CJSON::decode(file_get_contents($messageFile)) : []);
+  	if(!is_array($messages)) $messages=[];
+    
+  	if($cache) {
+  		$dependency=new CFileCacheDependency($messageFile);
+  		$cache->set($cacheKey, serialize($messages), $this->cachingDuration, $dependency);
+  	}
+    
+  	return $messages;
+  }
+}
