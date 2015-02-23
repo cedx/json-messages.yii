b701e559bc0c76a0a7c284fe69df0005a020b045
Cédric Belin <cedric@belin.io>
2015-02-23 22:34:18 +0100

    Fixed bugs

--- test/index.php
+++ test/index.php
@@ -13,11 +13,13 @@ define('YII_ENV', 'test');
 $rootPath=dirname(__DIR__);
 require_once $rootPath.'/vendor/autoload.php';
 require_once $rootPath.'/vendor/yiisoft/yii2/Yii.php';
-require_once $rootPath.'/lib/JsonMessageSource.php';
 
-// Initialize the test application.
+// Initialize the application.
+Yii::setAlias('@root', $rootPath);
+Yii::setAlias('@yii/i18n', '@root/lib');
+
 new Application([
   'id'=>'json-messages.yii',
-  'basePath'=>$rootPath.'/lib',
-  'vendorPath'=>$rootPath.'/vendor'
+  'basePath'=>'@root/lib',
+  'vendorPath'=>'@root/vendor'
 ]);
