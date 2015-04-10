cf2caebab8e1327d47d97a1c4d09333f125b0129
Cédric Belin <cedric@belin.io>
2015-04-10 11:25:25 +0200

    Allowing Yii environment to be overridden

--- test/index.php
+++ test/index.php
@@ -3,13 +3,13 @@
  * Entry point of the unit tests.
  * @module test.index
  */
- 
+
 // Module dependencies.
 use yii\console\Application;
 
 // Set the environment.
 define('YII_DEBUG', true);
-define('YII_ENV', 'test');
+define('YII_ENV', getenv('YII_ENV') ?: 'test');
 
 // Load the dependencies.
 $rootPath=dirname(__DIR__);
@@ -18,8 +18,6 @@ require_once $rootPath.'/vendor/yiisoft/yii2/Yii.php';
 
 // Initialize the application.
 Yii::setAlias('@root', $rootPath);
-Yii::setAlias('@yii/i18n', '@root/lib');
-
 new Application([
   'id'=>'json-messages.yii',
   'basePath'=>'@root/lib',
