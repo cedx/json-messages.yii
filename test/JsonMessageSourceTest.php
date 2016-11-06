69e669ee64929c2098b95c496b4ff1c0365552c7
Cédric Belin <cedric@belin.io>
2016-11-06 02:43:00 +0100

    Removed the `JsonMessageSourceStub` class

--- test/JsonMessageSourceTest.php
+++ test/JsonMessageSourceTest.php
@@ -6,37 +6,12 @@ namespace yii\test\i18n;
 use yii\i18n\{JsonMessageSource};
 
 /**
- * Publicly exposes the features of the `yii\i18n\JsonMessageSource` class.
- */
-class JsonMessageSourceStub extends JsonMessageSource {
-
-  /**
-   * Returns message file path for the specified language and category.
-   * @param string $category The message category.
-   * @param string $language The target language.
-   * @return string The path to message file.
-   */
-  public function getMessageFilePath($category, $language): string {
-    return parent::getMessageFilePath($category, $language);
-  }
-
-  /**
-   * Loads the message translation for the specified language and category.
-   * @param string $messageFile string The path to message file.
-   * @return string[] The message array or `null` if the file is not found.
-   */
-  public function loadMessagesFromFile($messageFile): array {
-    return parent::loadMessagesFromFile($messageFile);
-  }
-}
-
-/**
  * Tests the features of the `yii\i18n\JsonMessageSource` class.
  */
 class JsonMessageSourceTest extends \PHPUnit_Framework_TestCase {
 
   /**
-   * @var JsonMessageSourceStub The data context of the tests.
+   * @var JsonMessageSource The data context of the tests.
    */
   private $model;
 
@@ -44,16 +19,24 @@ class JsonMessageSourceTest extends \PHPUnit_Framework_TestCase {
    * Tests the `JsonMessageSource::getMessageFilePath()` method.
    */
   public function testGetMessageFile() {
+    $getMessageFilePath = function($category, $language) {
+      return $this->getMessageFilePath($category, $language);
+    };
+
     $expected = str_replace('/', DIRECTORY_SEPARATOR, $this->model->basePath . '/fr/messages.json');
-    $this->assertEquals($expected, $this->model->getMessageFilePath('messages', 'fr'));
+    $this->assertEquals($expected, $getMessageFilePath->call($this->model, 'messages', 'fr'));
   }
 
   /**
    * Tests the `JsonMessageSource::loadMessagesFromFile()` method.
    */
   public function testLoadMessagesFromFile() {
+    $loadMessagesFromFile = function($messageFile) {
+      return $this->loadMessagesFromFile($messageFile);
+    };
+
     $expected = [ 'Hello World!' => 'Bonjour le monde !' ];
-    $this->assertEquals($expected, $this->model->loadMessagesFromFile($this->model->basePath . '/fr/messages.json'));
+    $this->assertEquals($expected, $loadMessagesFromFile->call($this->model, $this->model->basePath . '/fr/messages.json'));
     $this->assertEquals('Bonjour le monde !', $this->model->translate('messages', 'Hello World!', 'fr'));
   }
 
@@ -61,7 +44,7 @@ class JsonMessageSourceTest extends \PHPUnit_Framework_TestCase {
    * Performs a common set of tasks just before each test method is called.
    */
   protected function setUp() {
-    $this->model = new JsonMessageSourceStub();
+    $this->model = new JsonMessageSource();
     $this->model->basePath = __DIR__;
   }
 }
