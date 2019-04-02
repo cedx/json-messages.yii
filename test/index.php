7f0c8c3930a9c3571a5338f7a2f2ec06da8d85e4
Cédric Belin <cedric@belin.io>
2019-04-02 10:56:29 +0200

    Renamed the 'lib' folder to 'src'

--- test/index.php
+++ test/index.php
@@ -10,7 +10,7 @@ $rootPath = dirname(__DIR__);
 require_once "$rootPath/vendor/autoload.php";
 require_once "$rootPath/vendor/yiisoft/yii2/Yii.php";
 Yii::setAlias('@root', $rootPath);
-Yii::setAlias('@yii/i18n', "$rootPath/lib");
+Yii::setAlias('@yii/i18n', "$rootPath/src");
 
 // Start the application.
-new Application(['id' => 'yii2-json-messages', 'basePath' => '@root/lib']);
+new Application(['id' => 'yii2-json-messages', 'basePath' => '@root/src']);
