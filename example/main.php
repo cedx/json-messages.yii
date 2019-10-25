dd3c028a8c3ce5b47586f00da171ff40a8a40186
Cédric Belin <cedric@belin.io>
2019-10-25 10:56:31 +0200

    Using typed properties

--- example/main.php
+++ example/main.php
@@ -4,10 +4,10 @@ use yii\i18n\{JsonMessageSource};
 /** Translates a message. */
 function main(): void {
   // Using flat mapping.
-  \Yii::$app->i18n->translations['app*'] = new JsonMessageSource;
-  echo \Yii::t('app', 'FooBarBaz');
+  Yii::$app->i18n->translations['app*'] = new JsonMessageSource;
+  echo Yii::t('app', 'FooBarBaz');
 
   // Using nested objects.
-  \Yii::$app->i18n->translations['app*'] = new JsonMessageSource(['enableNesting' => true]);
-  echo \Yii::t('app', 'foo.bar.baz');
+  Yii::$app->i18n->translations['app*'] = new JsonMessageSource(['enableNesting' => true]);
+  echo Yii::t('app', 'foo.bar.baz');
 }
